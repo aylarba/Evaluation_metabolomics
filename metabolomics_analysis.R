903997483cdc38d7f9fbab8cbb39cbdb1e447ad0
Aylar <88648825+aylarba@users.noreply.github.com>
2024-03-24 08:51:51 -0400

    Add files via upload

---
+++ metabolomics_analysis.R
@@ -0,0 +1,44 @@
+
+# Install packages if you haven't already
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+BiocManager::install(c("MSnbase", "xcms", "metaboAnalyst", "ggplot2"))
+
+# Load packages
+library(MSnbase)
+library(xcms)
+library(metaboAnalyst)
+library(ggplot2)
+
+# Reading the Data
+data_tool1 <- read.csv("path/to/your/dataset1.csv", row.names = 1)
+data_tool2 <- read.csv("path/to/your/dataset2.csv", row.names = 1)
+
+# Basic Data Exploration
+summary(data_tool1)
+summary(data_tool2)
+
+sum(is.na(data_tool1))
+sum(is.na(data_tool2))
+
+# Statistical Analysis
+results_ttest <- apply(data_tool1, 2, function(x) {
+    t.test(x ~ group)  # 'group' needs to be defined in your dataset
+})
+
+p_values <- sapply(results_ttest, function(x) x$p.value)
+
+# Multivariate Analysis (PCA, Clustering)
+pca_res <- prcomp(t(data_tool1), center = TRUE, scale. = TRUE)
+plot(pca_res$x[,1], pca_res$x[,2], col = as.factor(groups))  # 'groups' needs to be defined
+
+dist_matrix <- dist(t(data_tool1))
+hc_res <- hclust(dist_matrix)
+plot(hc_res)
+
+# Visualization
+ggplot(data.frame(logFC=logFC, pval=p_values), aes(x=logFC, y=-log10(pval))) +
+    geom_point() +
+    theme_minimal() +
+    labs(x="Log Fold Change", y="-Log10(p-value)", title="Volcano Plot")
